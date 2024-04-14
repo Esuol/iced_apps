2b886c72a2625f873254dd39af12f03349a559c0
berlinen <1292393108@qq.com>
2024-04-14 18:15:43 +0800

    feat: impl Arc

--- arc/src/main.rs
+++ arc/src/main.rs
@@ -30,6 +30,73 @@ impl Arc {
     }
 }
 
-pub fn main() {
-    println!("Hello, world!");
+impl Default for Arc {
+    fn default() -> Self {
+        Arc {
+            start: Instant::now(),
+            cache: Cache::default(),
+        }
+    }
+}
+
+impl<Message> canvas::Program<Message> for Arc {
+    type State = ();
+
+    fn draw(
+        &self,
+        _state: &Self::State,
+        renderer: &Renderer,
+        theme: &Theme,
+        bounds: Rectangle,
+        _cursor: mouse::Cursor,
+    ) -> Vec<Geometry> {
+        let geometry = self.cache.draw(renderer, bounds.size(), |frame| {
+            let palette = theme.palette();
+
+            let center = frame.center();
+            let radius = frame.width().min(frame.height()) / 5.0;
+
+            let start = Point::new(center.x, center.y - radius);
+
+            let angle = (self.start.elapsed().as_millis() % 10_000) as f32 / 10_000.0 * 2.0 * PI;
+
+            let end = Point::new(
+                center.x + radius * angle.cos(),
+                center.y + radius * angle.sin(),
+            );
+
+            let circles = Path::new(|b| {
+                b.circle(start, 10.0);
+                b.move_to(end);
+                b.circle(end, 10.0);
+            });
+
+            frame.fill(&circles, palette.text);
+
+            let path = Path::new(|b| {
+                b.move_to(start);
+                b.arc_to(center, end, 50.0);
+                b.line_to(end);
+            });
+
+            frame.stroke(
+                &path,
+                Stroke {
+                    style: stroke::Style::Solid(palette.text),
+                    width: 10.0,
+                    ..Stroke::default()
+                },
+            );
+        });
+
+        vec![geometry]
+    }
+}
+
+pub fn main() -> iced::Result {
+    iced::program("Arc - Iced", Arc::update, Arc::view)
+        .subscription(Arc::subscription)
+        .theme(|_| Theme::Dark)
+        .antialiasing(true)
+        .run()
 }
