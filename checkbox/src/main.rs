1354dc62b9013f2117b918334dc4e37be35764ba
berlinen <1292393108@qq.com>
2024-04-14 15:33:37 +0800

    feat: update checkbox struct

--- checkbox/src/main.rs
+++ checkbox/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{checkbox, column, container, row, text};
+use iced::{Element, Font, Length};
+
+const ICON_FONT: Font = Font::with_name("icons");
+
+#[derive(Default)]
+struct Example {
+    default: bool,
+    styled: bool,
+    custom: bool,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    DefaultToggled(bool),
+    StyledToggled(bool),
+    CustomToggled(bool),
+}
+
+pub fn main() {
+    print!("hello")
 }
