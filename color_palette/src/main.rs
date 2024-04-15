fddb7eae67ea9f6e914742bdb23102589bfd9dc3
berlinen <1292393108@qq.com>
2024-04-15 21:33:52 +0800

    feat: impl color_palette

---
+++ color_palette/src/main.rs
@@ -0,0 +1,12 @@
+use iced::alignment::{self, Alignment};
+use iced::mouse;
+use iced::widget::canvas::{self, Canvas, Frame, Geometry, Path};
+use iced::widget::{column, row, text, Slider};
+use iced::{
+    Color, Element, Font, Length, Pixels, Point, Rectangle, Renderer, Size, Vector,
+}
+use plaette::{convert::FromColor, rgb::Rgb, Darken, Hsl, Lighten, ShiftHue, Saturate, Shade};
+
+fn main() {
+    println!("Hello, world!");
+}
