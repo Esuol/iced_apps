979b89890276bb1e3f71973c5fdb6bfad5a4be44
berlinen <1292393108@qq.com>
2024-04-15 21:51:19 +0800

    feat: init color_palette

--- color_palette/src/main.rs
+++ color_palette/src/main.rs
@@ -1,12 +1,3 @@
-use iced::alignment::{self, Alignment};
-use iced::mouse;
-use iced::widget::canvas::{self, Canvas, Frame, Geometry, Path};
-use iced::widget::{column, row, text, Slider};
-use iced::{
-    Color, Element, Font, Length, Pixels, Point, Rectangle, Renderer, Size, Vector,
-}
-use plaette::{convert::FromColor, rgb::Rgb, Darken, Hsl, Lighten, ShiftHue, Saturate, Shade};
-
 fn main() {
     println!("Hello, world!");
 }
