1701f14f307e5510e9b6c7b693522716ac509a95
berlinen <1292393108@qq.com>
2024-04-15 10:48:16 +0800

    feat: mod bezier

--- bezier_tool/src/main.rs
+++ bezier_tool/src/main.rs
@@ -1,3 +1,20 @@
+use iced::widget::{button, column, text};
+use iced::{Alignment, Element, Length};
+
+mod bezier;
+
+#[derive(Default)]
+struct Example {
+    bezier: bezier::State,
+    curves: Vec<bezier::Curve>,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    AddCurve(bezier::Curve),
+    Clear,
+}
+
 fn main() {
     println!("Hello, world!");
 }
