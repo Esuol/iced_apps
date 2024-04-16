ebfb0e93909a4ccefe7f6c01422a5ac4e7e956a5
berlinen <1292393108@qq.com>
2024-04-16 16:34:45 +0800

    feat: component numeric_input update

--- component/src/main.rs
+++ component/src/main.rs
@@ -1,6 +1,13 @@
 use iced::widget::container;
 use iced::{Element, Length};
 
+mod numeric_input;
+
+#[derive(Default)]
+pub struct Component {
+    value: Option<u32>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
--- component/src/numeric_input.rs
+++ component/src/numeric_input.rs
@@ -1,7 +1,6 @@
 use iced::alignment::{self, Alignment};
 use iced::widget::{button, component, row, text, text_input, Component};
 use iced::{Element, Length, Size};
-
 pub struct NumericInput<Message> {
     value: Option<u32>,
     on_change: Box<dyn Fn(Option<u32>) -> Message>,
@@ -12,4 +11,50 @@ pub fn numeric_input<Message>(
     on_change: impl Fn(Option<u32>) -> Message + 'static,
 ) -> NumericInput<Message> {
     NumericInput::new(value, on_change)
+}
+
+#[derive(Debug, Clone)]
+pub enum Event {
+    InputChanged(String),
+    IncrementPressed,
+    DecrementPressed,
+}
+
+impl<Message> NumericInput<Message> {
+    pub fn new(value: Option<u32>, on_change: impl Fn(Option<u32>) -> Message + 'static) -> Self {
+        Self {
+            value,
+            on_change: Box::new(on_change),
+        }
+    }
+}
+
+impl<Message, Theme> Component<Message, Theme> for NumericInput<Message>
+where
+    Theme: text::Catalog + button::Catalog + text_input::Catalog + 'static,
+{
+    type State = ();
+    type Event = Event;
+
+    fn update(&mut self, _state: &mut Self::State, event: Self::Event) -> Option<Message> {
+        match event {
+            Event::IncrementPressed => Some((self.on_change)(Some(
+                self.value.unwrap_or_default().saturating_add(1),
+            ))),
+            Event::DecrementPressed => Some((self.on_change)(Some(
+                self.value.unwrap_or_default().saturating_sub(1),
+            ))),
+            Event::InputChanged(value) => {
+                if value.is_empty() {
+                    Some((self.on_change)(None))
+                } else {
+                    value
+                        .parse()
+                        .ok()
+                        .map(Some)
+                        .map(self.on_change.as_ref())
+                }
+            }
+        }
+    }
 }
