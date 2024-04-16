579a64cfdeb06522488fda8542edfe504f324e83
berlinen <1292393108@qq.com>
2024-04-16 17:13:36 +0800

    feat: component impl

--- component/src/main.rs
+++ component/src/main.rs
@@ -2,6 +2,7 @@ use iced::widget::container;
 use iced::{Element, Length};
 
 mod numeric_input;
+pub use numeric_input::numeric_input;
 
 #[derive(Default)]
 struct Component {
@@ -14,9 +15,23 @@ enum Message {
 }
 
 impl Component {
-    
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::NumericInputChanged(value) => {
+                self.value = value;
+            }
+        }
+    }
+
+    fn view(&self) -> Element<Message> {
+        container(numeric_input(self.value, Message::NumericInputChanged))
+            .padding(20)
+            .height(Length::Fill)
+            .center_y()
+            .into()
+    }
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> iced::Result {
+    iced::run("Component - Iced", Component::update, Component::view)
 }
