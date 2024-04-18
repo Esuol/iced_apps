74aa4dc44243b806a84a670a0ae94d642cd7f9cf
berlinen <1292393108@qq.com>
2024-04-18 14:59:12 +0800

    feat: widget define State

--- custom_widget/src/main.rs
+++ custom_widget/src/main.rs
@@ -1,4 +1,21 @@
+use iced::widget::{column, container, slider, text};
+use iced::{Alignment, Element, Length};
+
 mod circle;
+use circle::Circle;
+
+struct Example {
+    radius: f32,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    RadiusChanged(f32),
+}
+
+impl Example {
+    
+}
 
 fn main() {
     println!("Hello, world!");
