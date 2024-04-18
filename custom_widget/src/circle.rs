4ab59e0a6feb8ee89a8baa6df99d9c2e23ed68eb
berlinen <1292393108@qq.com>
2024-04-18 14:56:47 +0800

    feat: custom widget impl Circle

--- custom_widget/src/circle.rs
+++ custom_widget/src/circle.rs
@@ -20,12 +20,49 @@ pub fn circle(radius: f32) -> Circle {
 
 impl<Message, Theme, Renderer> Widget<Message, Theme, Renderer> for Circle
 where
-  Renderer: renderer::Renderer,
+    Renderer: renderer::Renderer,
 {
-  fn size(&self) -> Size<Length> {
-    Size {
-      width: Length::Shrink,
-      height: Length::Shrink
+    fn size(&self) -> Size<Length> {
+        Size {
+            width: Length::Shrink,
+            height: Length::Shrink,
+        }
     }
-  }
+
+    fn layout(
+        &self,
+        _tree: &mut widget::Tree,
+        _renderer: &Renderer,
+        _limits: &layout::Limits,
+    ) -> layout::Node {
+        layout::Node::new(Size::new(self.radius * 2.0, self.radius * 2.0))
+    }
+
+    fn draw(
+        &self,
+        _state: &widget::Tree,
+        renderer: &mut Renderer,
+        _theme: &Theme,
+        _style: &renderer::Style,
+        layout: Layout<'_>,
+        _cursor: mouse::Cursor,
+        _viewport: &Rectangle,
+    ) {
+        renderer.fill_quad(
+            renderer::Quad {
+                bounds: layout.bounds(),
+                border: Border::rounded(self.radius),
+                ..renderer::Quad::default()
+            },
+            Color::BLACK,
+        )
+    }
+}
+impl<'a, Message, Theme, Renderer> From<Circle> for Element<'a, Message, Theme, Renderer>
+where
+    Renderer: renderer::Renderer,
+{
+    fn from(circle: Circle) -> Self {
+        Self::new(circle)
+    }
 }
