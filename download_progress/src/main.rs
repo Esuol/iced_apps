0d0020e5d47a7cb7e83256c29be500618e970b92
berlinen <1292393108@qq.com>
2024-04-19 15:50:43 +0800

    feat: impl State

--- download_progress/src/main.rs
+++ download_progress/src/main.rs
@@ -1,5 +1,56 @@
 mod download;
 
+use iced::overlay::menu::State;
+use iced::widget::{button, column, container, progress_bar, text, Column};
+use iced::{Alignment, Element, Length, Subscription};
+
+#[derive(Debug, Clone)]
+struct Example {
+    downloads: Vec<Download>,
+    last_id: usize,
+}
+
+#[derive(Debug)]
+pub enum Message {
+    Add,
+    Download(usize),
+    DownloadProgressed((usize, download::Progress)),
+}
+
+#[derive(Debug)]
+struct Download {
+    id: usize,
+    state: State,
+}
+
+#[derive(Debug)]
+enum State {
+    Idle,
+    Downloading { progress: f32 },
+    Finished,
+    Errored,
+}
+
+impl Download {
+    pub fn new(id: usize) -> Self {
+        Self {
+            id,
+            state: State::Idle,
+        }
+    }
+
+    pub fn start(&mut self) {
+        match  self.state {
+            State::Idle { .. }
+                | State::Finished {..}
+                | State::Errored {..} => {
+                    self.state = State::Downloading { progress: 0.0 };
+                }
+                State::Downloading { .. } => {}
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
