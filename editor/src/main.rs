8e42cff30bcaa8bfe9ff6abe6e3eb85442c411af
berlinen <1292393108@qq.com>
2024-04-20 13:53:20 +0800

    feat: init editor main

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -1,3 +1,15 @@
+use iced::highlighter::{self, highlighter};
+use iced::keyboard;
+use iced::widget::{
+    button, column, container, horizotal_scroll, pick_list, row, text, text_editor, tooltip
+}
+use iced::{Alignment, Command, Element, Font, Length, Subscription, Theme};
+
+use std::ffi;
+use std::io;
+use std::{Path, PathBuf};
+use std::sync::Arc;
+
 fn main() {
     println!("Hello, world!");
 }
