585c3872ccba0940878748eef592e25065db6763
berlinen <1292393108@qq.com>
2024-04-21 19:54:28 +0800

    feat: events define Events

--- events/src/main.rs
+++ events/src/main.rs
@@ -1,3 +1,22 @@
+use iced::alignment;
+use iced::event::{self, Event};
+use iced::widget::{button, checkbox, container, text, Column};
+use iced::window;
+use iced::{Alignment, Command, Element, Length, Subscription};
+
+#[derive(Debug, Default)]
+struct Events {
+    last: Vec<Event>,
+    enabled: bool,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    EventOccurred(Event),
+    Toggled(bool),
+    Exit,
+}
+
 fn main() {
     println!("Hello, world!");
 }
