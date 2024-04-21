b1c46281e48d1b2729f564e5373a22571cf496ad
berlinen <1292393108@qq.com>
2024-04-21 19:59:05 +0800

    feat: events impl Events

--- events/src/main.rs
+++ events/src/main.rs
@@ -17,6 +17,37 @@ enum Message {
     Exit,
 }
 
+impl Events {
+    fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::EventOccurred(event) => {
+                self.last.push(event);
+
+                if self.last.len() > 5 {
+                    let _ = self.last.remove(0);
+                }
+
+                Command::none()
+            }
+
+            Message::EventOccurred(event) => {
+                if let Event::Window(id, window::Event::CloseRequested) = event {
+                    window::close(id)
+                } else {
+                    Command::none()
+                }
+            }
+
+            Message::Toggled(enabled) => {
+                self.enabled = enabled;
+                Command::none()
+            }
+
+            Message::Exit => window::close(window::Id::MAIN),
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
