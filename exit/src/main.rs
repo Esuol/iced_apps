adb27de46b91fce2c37a2317010da40783ab7a71
berlinen <1292393108@qq.com>
2024-04-22 10:27:15 +0800

    feat: exit impl update

--- exit/src/main.rs
+++ exit/src/main.rs
@@ -1,3 +1,31 @@
+use iced::widget::{button, column, container};
+use iced::window;
+use iced::{Alignment, Command, Element, Length};
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[derive(Default)]
+struct Exit {
+    show_confirm: bool,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    Confirm,
+    Exit,
+}
+
+impl Exit {
+    fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::Confirm => window::close(window::Id::MAIN),
+            Message::Exit => {
+                self.show_confirm = true;
+
+                Command::none()
+            }
+        }
+    }
 }
