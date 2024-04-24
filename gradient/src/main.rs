8404b979ba22bc69699645cac3dfc7ed432ee847
berlinen <1292393108@qq.com>
2024-04-24 10:00:57 +0800

    feat: init gradient;

---
+++ gradient/src/main.rs
@@ -0,0 +1,8 @@
+use iced::gradient;
+use iced::widget::{checkbox, column, container, horizontal_space, row, slider, text};
+use iced::Program;
+use iced::{Alignment, Color, Element, Length, Radians, Theme};
+
+fn main() {
+    println!("Hello, world!");
+}
