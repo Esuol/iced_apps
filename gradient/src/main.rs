491bc6de6fbdfdc7b8513768f381113371934870
berlinen <1292393108@qq.com>
2024-04-25 09:57:36 +0800

    feat: Gradient view

--- gradient/src/main.rs
+++ gradient/src/main.rs
@@ -28,6 +28,59 @@ impl Gradient {
             transparent: false,
         }
     }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::StartChanged(color) => self.start = color,
+            Message::EndChanged(color) => self.end = color,
+            Message::AngleChanged(angle) => self.angle = angle,
+            Message::TransparentToggled(transparent) => {
+                self.transparent = transparent;
+            }
+        }
+    }
+
+    fn view(&self) -> Element<Message> {
+        let Self {
+            start,
+            end,
+            angle,
+            transparent,
+        } = *self;
+
+        let gradient_box = container(horizontal_space())
+            .style(move |_theme| {
+                let gradient = gradient::Linear::new(angle)
+                    .add_stop(0.0, start)
+                    .add_stop(1.0, end);
+
+                gradient.into()
+            })
+            .width(Length::Fill)
+            .height(Length::Fill);
+
+        let angle_picker = row![
+            text("Angle").width(64),
+            slider(Radians::RANGE, self.angle, Message::AngleChanged).step(0.01)
+        ]
+        .spacing(8)
+        .padding(8)
+        .align_items(Alignment::Center);
+
+        let transparency_toggle = iced::widget::Container::new(
+            checkbox("Transparent window", transparent).on_toggle(Message::TransparentToggled),
+        )
+        .padding(8);
+
+        column![
+            color_picker("Start", self.start).map(Message::StartChanged),
+            color_picker("End", self.end).map(Message::EndChanged),
+            angle_picker,
+            transparency_toggle,
+            gradient_box,
+        ]
+        .into()
+    }
 }
 
 fn main() {
