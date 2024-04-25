8cd3154f5e6d7d1e075afbce869ca3a593cafc11
berlinen <1292393108@qq.com>
2024-04-25 09:57:03 +0800

    feat: Gradient new

--- gradient/src/main.rs
+++ gradient/src/main.rs
@@ -3,6 +3,33 @@ use iced::widget::{checkbox, column, container, horizontal_space, row, slider, t
 use iced::Program;
 use iced::{Alignment, Color, Element, Length, Radians, Theme};
 
+#[derive(Debug, Clone, Copy)]
+struct Gradient {
+    start: Color,
+    end: Color,
+    angle: Radians,
+    transparent: bool,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    StartColorChanged(Color),
+    EndColorChanged(Color),
+    AngleChanged(Radians),
+    TransparentToggled(bool),
+}
+
+impl Gradient {
+    fn new() -> Self {
+        Self {
+            start: Color::BLACK,
+            end: Color::new(0.0, 0.0, 1.0, 1.0),
+            angle: Radians(0.0),
+            transparent: false,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
