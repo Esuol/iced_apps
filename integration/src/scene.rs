f70479044c532c72234208ec9fdbdeacac04d81b
berlinen <1292393108@qq.com>
2024-04-26 13:55:27 +0800

    feat: impl integration mod scene clear

--- integration/src/scene.rs
+++ integration/src/scene.rs
@@ -1,4 +1,4 @@
-use iced_wgpu::wgpu;
+use iced_wgpu::wgpu::{self, naga::back};
 use iced_winit::core::Color;
 
 pub struct Scene {
@@ -10,6 +10,36 @@ impl Scene {
         let pipeline = build_pipeline(device, texture_format);
 
         Scene { pipeline }
+    }
+
+    pub fn clear<'a>(
+      target: &'a wgpu::TextureView,
+      encoder: &'a mut wgpu::CommandEncoder,
+      background_color: Color,
+    ) -> wgpu::RenderPass<'a> {
+       encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+        label: None,
+        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+          view: target,
+          resolve_target: None,
+          ops: wgpu::Operations {
+            load: wgpu::LoadOp::Clear({
+              let [r,g,b, a] =background_color.into_linear();
+
+              wgpu::Color {
+                r: r as f64,
+                g: g as f64,
+                b: b as f64,
+                a: a as f64,
+              }
+            }),
+            store: wgpu::StoreOp::Store,
+          },
+        })],
+        depth_stencil_attachment: None,
+        timestamp_writes: None,
+        occlusion_query_set: None,
+       })
     }
 }
 
