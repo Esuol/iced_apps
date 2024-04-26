1f12f35666704550ac43717c5507471371f6aba5
berlinen <1292393108@qq.com>
2024-04-26 14:28:56 +0800

    feat: integration main

--- integration/src/main.rs
+++ integration/src/main.rs
@@ -1,6 +1,20 @@
 mod controls;
 mod scene;
 
+use controls::Controls;
+use iced_wgpu::graphics::Viewport;
+use iced_wgpu::{wgpu, Engine, Renderer};
+use iced_winit::core::{mouse, renderer, window, Color, Font, Pixels, Size, Theme};
+use iced_winit::runtime::{Debug, Program};
+use iced_winit::{conversion, futures, winit, Clipboard};
+use scene::Scene;
+
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
 fn main() {
     println!("Hello, world!");
 }
