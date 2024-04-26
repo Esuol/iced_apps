73e51e1b1af9d5f9fffd3a28ee5996cb788bfebb
berlinen <1292393108@qq.com>
2024-04-26 13:49:30 +0800

    feat: impl integration mod scene build_pipeline

--- integration/src/scene.rs
+++ integration/src/scene.rs
@@ -14,11 +14,51 @@ impl Scene {
 }
 
 fn build_pipeline(
-  device: &wgpu::Device,
-  texture_format: wgpu::TextureFormat,
+    device: &wgpu::Device,
+    texture_format: wgpu::TextureFormat,
 ) -> wgpu::RenderPipeline {
-  let (vs_module, fs_module) = (
-    device.create_shader_module(wgpu::include_wgsl!("shader/vert.wgsl")),
-    device.create_shader_module(wgpu::include_wgsl!("shader/frag.wgsl")),
-  );
+    let (vs_module, fs_module) = (
+        device.create_shader_module(wgpu::include_wgsl!("shader/vert.wgsl")),
+        device.create_shader_module(wgpu::include_wgsl!("shader/frag.wgsl")),
+    );
+
+    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: None,
+        push_constant_ranges: &[],
+        bind_group_layouts: &[],
+    });
+
+    device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label: None,
+        layout: Some(&pipeline_layout),
+        vertex: wgpu::VertexState {
+            module: &vs_module,
+            entry_point: "main",
+            buffers: &[],
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: &fs_module,
+            entry_point: "main",
+            targets: &[Some(wgpu::ColorTargetState {
+                format: texture_format,
+                blend: Some(wgpu::BlendState {
+                    color: wgpu::BlendComponent::REPLACE,
+                    alpha: wgpu::BlendComponent::REPLACE,
+                }),
+                write_mask: wgpu::ColorWrite::ALL,
+            })],
+        }),
+        primitive: wgpu::PrimitiveState {
+          topology: wgpu::PrimitiveTopology::TriangleList,
+          front_face: wgpu::FrontFace::Ccw,
+          ....Default::default()
+        },
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState {
+          count: 1,
+          mask: !0,
+          alpha_to_coverage_enabled: false,
+        },
+        multiview: None,
+    })
 }
