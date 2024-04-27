c549741192557baa230a2f7ef1883b8d70adc6b4
berlinen <1292393108@qq.com>
2024-04-27 13:32:16 +0800

    feat: init layout

---
+++ layout/src/main.rs
@@ -0,0 +1,14 @@
+use iced::keyboard;
+use iced::mouse;
+use iced::widget::{
+    button, canvas, checkbox, column, container, horizontal_space, pick_list, row, scrollable, text,
+};
+use iced::{
+    color, Alignment, Element, Font, Length, Point, Rectangle, Renderer, Subscription, Theme,
+};
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
