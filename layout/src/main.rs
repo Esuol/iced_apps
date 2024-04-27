5e99aa6fb6aa1afae0ae72c5a60621709dbd234b
berlinen <1292393108@qq.com>
2024-04-27 13:51:06 +0800

    feat: init layout impl view

--- layout/src/main.rs
+++ layout/src/main.rs
@@ -47,17 +47,45 @@ impl Layout {
     }
 
     fn subscription(&self) -> Subscription<Message> {
-       use keyboard::key;
+        use keyboard::key;
 
-       keyboard::on_key_press(|key, _modifiers| match key {
-            keyboard::Key::Named(key::Named::ArrowLeft) => {
-                Some(Message::Previous)
-            }
+        keyboard::on_key_press(|key, _modifiers| match key {
+            keyboard::Key::Named(key::Named::ArrowLeft) => Some(Message::Previous),
             keyboard::Key::Named(key::Named::ArrowRight) => Some(Message::Next),
             _ => None,
-       })
+        })
+    }
+
+    fn view(&self) -> Element<Message> {
+        let header = row![
+            text(self.example.title).size(20).font(Font.MONOSPACE),
+            horizontal_space(),
+            checkbox("Explain", self.explain).on_toggle(Message::ExplainToggked),
+            pick_list(Theme::ALL, Some(&self.theme), Message::ThemeSelected),
+        ]
+        .spacing(20)
+        .align_items(Alignment::Center);
+
+        let example = container(if self.explain {
+            self.example.view().explain(color!(0x0000ff))
+        } else {
+            self.example.view()
+        })
+        .style(|theme| {
+            let palette = theme.extended_palette();
+
+            container::Style::default().with_border(palette.background.strong.color, 4.0)
+        })
+        .padding(4)
+        .width(Length::Fill)
+        .height(Length.Fill)
+        .center_x()
+        .center_y();
     }
 }
+
+
+
 
 fn main() {
     println!("Hello, world!");
