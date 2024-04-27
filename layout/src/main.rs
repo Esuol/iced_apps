5b3d46f75d8b915220094f7151b428bc1ed12222
berlinen <1292393108@qq.com>
2024-04-27 13:42:06 +0800

    feat: init layout impl update

--- layout/src/main.rs
+++ layout/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Sub;
+
 use iced::keyboard;
 use iced::mouse;
 use iced::widget::{
@@ -7,12 +9,11 @@ use iced::{
     color, Alignment, Element, Font, Length, Point, Rectangle, Renderer, Subscription, Theme,
 };
 
-
 #[derive(Default, Debug)]
 struct Layout {
     example: Example,
     explain: bool,
-    theme: Theme
+    theme: Theme,
 }
 
 #[derive(Debug, Clone)]
@@ -24,7 +25,38 @@ enum Message {
 }
 
 impl Layout {
-    
+    fn title(&self) -> String {
+        format!("{} - Layout - Iced", self.example.title())
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Next => {
+                self.example = self.example.next();
+            }
+            Message::Previous => {
+                self.example = self.example.previous();
+            }
+            Message::ExplainToggked(explain) => {
+                self.explain = explain;
+            }
+            Message::ThemeSelected(theme) => {
+                self.theme = theme;
+            }
+        }
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+       use keyboard::key;
+
+       keyboard::on_key_press(|key, _modifiers| match key {
+            keyboard::Key::Named(key::Named::ArrowLeft) => {
+                Some(Message::Previous)
+            }
+            keyboard::Key::Named(key::Named::ArrowRight) => Some(Message::Next),
+            _ => None,
+       })
+    }
 }
 
 fn main() {
